961c4749fbed37cbd5bf97a3e8b425e83f6006b3
Juan <juseumca@gmail.com>
2023-06-22 09:28:43 -0500

    POO

---
+++ POO/clase/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace clase.Persona{
+    class Persona{
+        
+    private string ? idPersona;
+    private string ? nombre;
+    private string ? apellido;
+    private int ? edad;
+    private string ? Email;
+
+    public Persona(string idPersona, string nombre, string apellido, int edad, string email){
+
+        this.idPersona = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.edad = edad;
+        this.email = email;
+
+    }
+    public Persona(){}
+
+    public string idPersona { get; set;}
+    public string nombre { get; set;}
+    public string apellido { get; set;}
+    public int edad { get; set;}
+    public string correo { get; set;}
+
+
+
+    }
+}
---
+++ shop/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+
+/*
+internal class Producto{
+
+    private string name;
+    private decimal precio;
+    private int cantidad;
+
+    public Producto(string name, decimal precio, int cantidad){
+
+        this.name = name;
+        this.precio = precio;
+        this.cantidad = cantidad;
+
+    }
+
+    public string ObtenerNombre(){
+
+        return this.name;
+
+    }
+
+    public decimal ObtenerPrecio(){
+
+        return this.precio;
+
+    }
+
+    public int ObtenerCantidad(){
+
+        return this.cantidad;
+
+    }
+
+}
+
+partial class Program{
+
+    static ArrayList listaArray = new ArrayList();
+
+    static void Main(){
+
+        bool flag = false;
+        
+        do{
+
+            Console.WriteLine("\n----------Menu----------\n");
+            Console.WriteLine("1. Registrar un producto ");
+            Console.WriteLine("2. BUscar un producto ");
+            Console.WriteLine("3. Mostrar un producto ");
+            Console.WriteLine("4. Salir...");            
+            Console.WriteLine("Ingrese una opción.");
+            string opción = Console.ReadLine();
+
+            switch(opc){
+
+                case "1":
+                    RegistroProducto();
+                    break;
+                case "2":
+                    BuscarProducto();
+                    break;
+                case "3":
+                    MostrarProducto();
+                    break;
+                case "4":
+                    opc = true;
+                    break;
+                default:
+                    Console.WriteLine("Opción Invalido.");
+                    break;
+            }
+
+        }
+        
+        while (!flag);
+
+    }
+    static void RegistroProducto(){
+
+        Console.WriteLine("Registro Producto");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el precio del producto.");
+        decimal precio = Decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la cantidad");
+        int cantidad = int.Parse(Console.ReadLine());
+
+        Producto nuevoProducto = new Producto(nombre,precio,cantidad);
+        listaProductos.Add(nuevoProducto);
+
+        Console.WriteLine("Registro Exitoso");
+
+    }
+
+    static void BuscarProducto(string nombre, string precio, int cantidad){
+
+        Console.WriteLine("Ingrese el nombre del Producto a buscar");
+        string nombreBusqueda = Console.ReadLine();
+
+        Producto nuevoEncontrado = null;
+        foreach(Producto producto in listaProducto){
+
+            if(producto.ObtenerNombre() == nombreBusqueda) {
+
+                productoEncontrado = producto;
+                break;
+
+            }
+        }
+
+        if (productoEncontrado != null){
+
+            Console.WriteLine($"Producto encontrado: {productoEncontrado.ObtenerNombre()}");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+
+    }
+
+
+}
+ */
+internal class Program{
+
+    private static void Main(string[] args) {
+
+        String ? palabra;
+        List<String> Dinosaurios = new List<String>();
+        string[] Dinos = {
+            "Branchisaurus",
+            "Amargasaurus",
+            "Mamenchisaurus"
+        };
+
+        Dinosaurios.AddRange(Dinos);
+        Console.WriteLine("Ingrese el nombre del Dinosaurio a buscar");
+        palabra = Console.ReadLine();
+        Console.WriteLine(Dinosaurios.Exists(item => item.Equals(palabra)) ? "Se encontro el dinosaurio" : "No se encontro el dinosaurio");
+        Console.WriteLine(String.Join(",",Dinosaurios));
+        //Console.WriteLine(typeof(Dinosaurios));
+
+    }
+
+}
